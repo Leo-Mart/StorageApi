279f4c6e55668548d9ccde152a65639f9f065802
Martin Leo <martin.christoffer.leo@gmail.com>
2026-06-08 13:07:27 +0200

    Add storage model

---
+++ StorageApi/Models/Product.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StorageApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Price { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public string Shelf { get; set; } = string.Empty;
+        public int Count { get; set; }
+        public string Description { get; set; } = string.Empty;
+
+    }
+}
